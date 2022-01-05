0988169e6aa56e1fb34929514fde60f37989d2f0
Karen Nabarrete <karen.nabarrete.garcia@gmail.com>
2022-01-05 17:04:48 -0300

    Adicionado a lista de Cursos e Dom

---
+++ js/acoes.js
@@ -0,0 +1,13 @@
+let btnCadastroCurso = document.querySelector("#cadastro_curso");
+let btnSalvarCurso = document.querySelector("#salvar");
+let btnSalvarEdicaoCurso = document.querySelector("#salvar-edicao");
+let btnCancelarCadastroCurso = document.querySelector("#cancelar");
+
+var dadosCursos = [
+    {"imagem": "novoCursoImagem",
+    "nome": "novoCursoNome",
+    "id": "novoCursoId",
+    "descricao" : "novoCursoDescricao",
+    
+    }
+]
